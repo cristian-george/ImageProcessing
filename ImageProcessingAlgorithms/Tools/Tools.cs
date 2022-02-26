a0d73e3b38ebac667a42ab7eacf6324292faa707
Fieraru Cristian George <cristian.fieraru@student.unitbv.ro>
2022-02-26 16:54:44 +0200

    Add project files.

---
+++ ImageProcessingAlgorithms/Tools/Tools.cs
@@ -0,0 +1,147 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace ImageProcessingAlgorithms.Tools
+{
+    public class Tools
+    {
+        public static Image<Gray, byte> Invert(Image<Gray, byte> inputImage)
+        {
+            var result = new Image<Gray, byte>(inputImage.Size);
+            for (int y = 0; y < inputImage.Height; y++)
+            {
+                for (int x = 0; x < inputImage.Width; x++)
+                {
+                    result.Data[y, x, 0] = (byte)(255 - inputImage.Data[y, x, 0]);
+                }
+            }
+            return result;
+        }
+
+        public static Image<Bgr, byte> Invert(Image<Bgr, byte> inputImage)
+        {
+            var result = new Image<Bgr, byte>(inputImage.Size);
+            for (int y = 0; y < inputImage.Height; y++)
+            {
+                for (int x = 0; x < inputImage.Width; x++)
+                {
+                    result.Data[y, x, 0] = (byte)(255 - inputImage.Data[y, x, 0]);
+                    result.Data[y, x, 1] = (byte)(255 - inputImage.Data[y, x, 1]);
+                    result.Data[y, x, 2] = (byte)(255 - inputImage.Data[y, x, 2]);
+                }
+            }
+            return result;
+        }
+
+        public static Image<Bgr, byte> Copy(Image<Bgr, byte> image)
+        {
+            var result = new Image<Bgr, byte>(image.Size);
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    result.Data[y, x, 0] = (byte)(image.Data[y, x, 0]);
+                }
+            }
+            return image;
+        }
+
+        public static Image<Gray, byte> Copy(Image<Gray, byte> image)
+        {
+            var result = new Image<Gray, byte>(image.Size);
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    result.Data[y, x, 0] = (byte)(image.Data[y, x, 0]);
+                }
+            }
+            return image;
+        }
+
+        public static Image<Gray, byte> Convert(Image<Bgr, byte> coloredImage)
+        {
+            var grayImage = coloredImage.Convert<Gray, byte>();
+
+            return grayImage;
+        }
+
+        public static Image<Gray, byte> Thresholding(Image<Gray, byte> inputImage, int threshold)
+        {
+            Image<Gray, byte> result = new Image<Gray, byte>(inputImage.Size);
+            for (int y = 0; y < inputImage.Height; y++)
+            {
+                for (int x = 0; x < inputImage.Width; x++)
+                {
+                    if (threshold <= inputImage.Data[y, x, 0])
+                        result.Data[y, x, 0] = 255;
+                }
+            }
+            return result;
+        }
+
+        public static Image<Gray, byte> CropImage(Image<Gray, byte> inputImage, int leftTopX, int leftTopY, int rightBottomX, int rightBottomY)
+        {
+            Image<Gray, byte> result = new Image<Gray, byte>(rightBottomX - leftTopX, rightBottomY - leftTopY);
+
+            for (int y = 0; y < result.Height; y++)
+            {
+                for (int x = 0; x < result.Width; x++)
+                {
+                    result.Data[y, x, 0] = inputImage.Data[y + leftTopY, x + leftTopX, 0];
+                }
+            }
+
+            return result;
+        }
+     
+        public static Image<Bgr, byte> CropImage(Image<Bgr, byte> inputImage, int leftTopX, int leftTopY, int rightBottomX, int rightBottomY)
+        {
+            Image<Bgr, byte> result = new Image<Bgr, byte>(rightBottomX - leftTopX, rightBottomY - leftTopY);
+
+            for (int y = 0; y < result.Height; y++)
+            {
+                for (int x = 0; x < result.Width; x++)
+                {
+                    result.Data[y, x, 0] = inputImage.Data[y + leftTopY, x + leftTopX, 0];
+                    result.Data[y, x, 1] = inputImage.Data[y + leftTopY, x + leftTopX, 1];
+                    result.Data[y, x, 2] = inputImage.Data[y + leftTopY, x + leftTopX, 2];
+                }
+            }
+
+            return result;
+        }
+
+        public static Image<Gray, byte> MirrorVertically(Image<Gray, byte> inputImage)
+        {
+            Image<Gray, byte> result = new Image<Gray, byte>(inputImage.Size);
+
+            for (int y = 0; y < inputImage.Height; y++)
+            {
+                for (int x = 0; x < inputImage.Width; x++)
+                {
+                    result.Data[y, x, 0] = inputImage.Data[y, inputImage.Width - x - 1, 0];
+                }
+            }
+
+            return result;
+        }
+
+        public static Image<Bgr, byte> MirrorVertically(Image<Bgr, byte> inputImage)
+        {
+            Image<Bgr, byte> result = new Image<Bgr, byte>(inputImage.Size);
+
+            for (int y = 0; y < inputImage.Height; y++)
+            {
+                for (int x = 0; x < inputImage.Width; x++)
+                {
+                    result.Data[y, x, 0] = inputImage.Data[y, inputImage.Width - x - 1, 0];
+                    result.Data[y, x, 1] = inputImage.Data[y, inputImage.Width - x - 1, 1];
+                    result.Data[y, x, 2] = inputImage.Data[y, inputImage.Width - x - 1, 2];
+                }
+            }
+
+            return result;
+        }
+    }
+}
